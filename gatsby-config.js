9034891f883c06833957a0eed41d05d801bd202d
jossdz <carloscorrea.dev@gmail.com>
2019-06-03 00:16:03 -0500

    config

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Platziswag`,
+    description: `El mejor swag de platzi disponible para ti.`,
+    author: `@jossdz`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
