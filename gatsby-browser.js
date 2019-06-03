1ec5b0460eef0f60f5578b7cbb39359a14936cab
jossdz <carloscorrea.dev@gmail.com>
2019-06-02 21:07:30 -0500

    Layout en Gatsby

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,7 +1,4 @@
-/**
- * Implement Gatsby's Browser APIs in this file.
- *
- * See: https://www.gatsbyjs.org/docs/browser-apis/
- */
+const React = require("react")
+const Layout = require("./src/components/layout").default
 
-// You can delete this file if you're not using it
+exports.wrapRootElement = ({ element }) => <Layout>{element}</Layout>
