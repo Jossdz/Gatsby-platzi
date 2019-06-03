839205551ce863b025de4d7cad71cc20a6657e0a
jossdz <carloscorrea.dev@gmail.com>
2019-06-02 21:12:01 -0500

    Query en React

--- src/components/Jumbo.js
+++ src/components/Jumbo.js
@@ -1,12 +1,12 @@
 import React from "react"
 import { StyledJumbo } from "../styles/components"
 
-export default function Jumbo() {
+export default function Jumbo({ description }) {
   return (
     <StyledJumbo>
       <div>
         <h2>¡Consigue el mejor swag exclusivo y especial de Platzi!</h2>
-        <small>Piezas elegantes para los mejores estudiantes.</small>
+        <small>{description}</small>
       </div>
     </StyledJumbo>
   )
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,12 +1,26 @@
 import React from "react"
-import { Link } from "gatsby"
+import { Link, graphql } from "gatsby"
 import { Jumbo } from "../components"
 import { SEO } from "../components"
 
-const IndexPage = () => (
+export const query = graphql`
+  query GET_DESCRIPTION {
+    allSite {
+      edges {
+        node {
+          siteMetadata {
+            description
+          }
+        }
+      }
+    }
+  }
+`
+
+const IndexPage = ({ data }) => (
   <>
     <SEO title="Home" />
-    <Jumbo />
+    <Jumbo description={data.allSite.edges[0].node.siteMetadata.description} />
     <h1>Hi people</h1>
     <p>Welcome to your new Gatsby site.</p>
     <p>Now go build something great.</p>
