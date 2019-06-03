7d88b1e85958834faacc39abd4053bb410d25ccb
jossdz <carloscorrea.dev@gmail.com>
2019-06-03 00:05:24 -0500

    Jumbo

---
+++ src/components/Jumbo.1.js
@@ -0,0 +1,13 @@
+import React from "react"
+import { StyledJumbo } from "../styles/components"
+
+export default function Jumbo() {
+  return (
+    <StyledJumbo>
+      <div>
+        <h2>¡Consigue el mejor swag exclusivo y especial de Platzi!</h2>
+        <small>Piezas elegantes para los mejores estudiantes.</small>
+      </div>
+    </StyledJumbo>
+  )
+}
