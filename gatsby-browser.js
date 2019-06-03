65d9d1915e9d46542064da88775f05282d0ac8b4
jossdz <carloscorrea.dev@gmail.com>
2019-06-03 00:28:43 -0500

    global styles

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,4 +1,10 @@
 const React = require("react")
 const Layout = require("./src/components/layout").default
+const { GlobalStyles } = require("./src/styles")
 
-exports.wrapRootElement = ({ element }) => <Layout>{element}</Layout>
+exports.wrapRootElement = ({ element }) => (
+  <>
+    <GlobalStyles />
+    <Layout>{element}</Layout>
+  </>
+)
