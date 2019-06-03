f4acb3ddae4f1b7dcd3131953ecbf3efba6dc769
jossdz <carloscorrea.dev@gmail.com>
2019-06-03 00:08:22 -0500

    layout-header

--- src/components/header.js
+++ src/components/header.js
@@ -1,34 +1,34 @@
 import { Link } from "gatsby"
 import PropTypes from "prop-types"
 import React from "react"
+import { MenuItem, StyledHeader } from "../styles/components"
 
-const Header = ({ siteTitle }) => (
-  <header
-    style={{
-      background: `rebeccapurple`,
-      marginBottom: `1.45rem`,
-    }}
-  >
-    <div
-      style={{
-        margin: `0 auto`,
-        maxWidth: 960,
-        padding: `1.45rem 1.0875rem`,
-      }}
-    >
-      <h1 style={{ margin: 0 }}>
-        <Link
-          to="/"
-          style={{
-            color: `white`,
-            textDecoration: `none`,
-          }}
-        >
-          {siteTitle}
-        </Link>
-      </h1>
-    </div>
-  </header>
+const Header = () => (
+  <StyledHeader>
+    <Link to="/">
+      <img src="https://i.postimg.cc/6q3pg48v/Logo.png" alt="logoplatziswag" />
+    </Link>
+    <nav>
+      <ul>
+        <MenuItem margin>
+          <Link to="/">Productos</Link>
+        </MenuItem>
+        <MenuItem margin>
+          <a href="http://www.platzi.com">Platzi</a>
+        </MenuItem>
+        <MenuItem>
+          <Link to="/cart">
+            <span>
+              <img
+                src="https://i.postimg.cc/L6wpMxLt/cart.png"
+                alt="cartlogo"
+              />
+            </span>
+          </Link>
+        </MenuItem>
+      </ul>
+    </nav>
+  </StyledHeader>
 )
 
 Header.propTypes = {
