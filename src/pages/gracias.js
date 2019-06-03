f7b6ece927f9b0ebed364a979d50bc44183d29ba
jossdz <carloscorrea.dev@gmail.com>
2019-06-02 20:42:09 -0500

    Router en Gatsby y Componente Link

---
+++ src/pages/gracias.js
@@ -0,0 +1,23 @@
+import React from "react"
+import { SEO } from "../components"
+import { Button, Purchase } from "../styles/components"
+import { Link } from "gatsby"
+
+export default () => {
+  return (
+    <>
+      <SEO title="Compra Exitosa" />
+      <Purchase>
+        <h2>Compra Exitosa</h2>
+        <p>Espero que disfrutes tu swag, lucelo con orgullo</p>
+        <p>¡Te esperamos de vuelta, no pares de aprender!</p>
+        <span rol="img" aria-label="emoji">
+          ❤️
+        </span>
+        <Link to="/">
+          <Button>Volver al Catálogo</Button>
+        </Link>
+      </Purchase>
+    </>
+  )
+}
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,15 +1,16 @@
 import React from "react"
 import { Link } from "gatsby"
-
+import { Jumbo } from "../components"
 import { Layout, SEO } from "../components"
 
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
+    <Jumbo />
     <h1>Hi people</h1>
     <p>Welcome to your new Gatsby site.</p>
     <p>Now go build something great.</p>
-    <Link to="/page-2/">Go to page 2</Link>
+    <Link to="/gracias/">Go to gracias</Link>
   </Layout>
 )
 
