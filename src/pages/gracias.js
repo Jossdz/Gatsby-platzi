4947eea6c6a31a06bea8f454b5954e9850cb4996
Carlos Cando <carloscando12@hotmail.com>
2019-12-14 19:11:17 -0500

    Creacion de la pagina 'gracias'

---
+++ src/pages/gracias.js
@@ -0,0 +1,22 @@
+import React from "react"
+import { SEO } from "../components"
+import { Button, Purchase } from "../styles/components"
+import { Link } from "gatsby"
+export default function gracias() {
+  return (
+    <div>
+      <SEO title="Compra Exitosa" />
+      <Purchase>
+        <h2>Compra Exitosa</h2>
+        <p>Espero que disfrutes de tu compra</p>
+        <p>¡Te esperamos de vuelta!</p>
+        <span rol="img" arial-label="emoji">
+          ❤️
+        </span>
+        <Link to="/">
+          <Button>Volver al Catálogo</Button>
+        </Link>
+      </Purchase>
+    </div>
+  )
+}
