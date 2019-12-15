883ec40e75a2df61987794f39eb48a0f2b2346df
Carlos Cando <carloscando12@hotmail.com>
2019-12-15 13:33:49 -0500

    Configuracion de Spire y dotenv en gatsby-config.js y el manejo de clave publica y privada en env.development

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,3 +1,7 @@
+require("dotenv").config({
+  path: `.env.${process.env.NODE_ENV}`,
+})
+
 module.exports = {
   siteMetadata: {
     title: `Pasarela`,
@@ -6,6 +10,7 @@ module.exports = {
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
+    `gatsby-plugin-stripe`,
     {
       resolve: `gatsby-source-filesystem`,
       options: {
@@ -31,6 +36,13 @@ module.exports = {
       resolve: `gatsby-plugin-typography`,
       options: {
         pathToConfigModule: "src/utils/typography.js",
+      },
+    },
+    {
+      resolve: `gatsby-source-stripe`,
+      options: {
+        objects: ["Sku"],
+        secretKey: process.env.STRIPE_SK,
       },
     },
     // this (optional) plugin enables Progressive Web App + Offline functionality
