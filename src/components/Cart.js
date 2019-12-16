d954166321433db86f592357531937d6e88d095d
Carlos Cando <carloscando12@hotmail.com>
2019-12-15 20:43:19 -0500

    Configuracion en /src/components/Cart.js para agregar productos al carrito y calcular el precio total de la compra

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -1,4 +1,4 @@
-import React, { useContext } from "react"
+import React, { useContext, useEffect, useState } from "react"
 import { Link } from "gatsby"
 import { Button, StyledCart } from "../styles/components"
 import priceFormat from "../utils/priceFormat"
@@ -6,6 +6,18 @@ import { CartContext } from "../context"
 
 export default function Cart() {
   const { cart } = useContext(CartContext)
+  const [total, setTotal] = useState(0)
+
+  const getTotal = () => {
+    setTotal(
+      cart.reduce((acc, current) => acc + current.price * current.quantity, 0)
+    )
+  }
+
+  useEffect(() => {
+    getTotal()
+  }, [])
+
   return (
     <StyledCart>
       <h2>Carrito de compras</h2>
@@ -32,7 +44,7 @@ export default function Cart() {
       <nav>
         <div>
           <h3>Subtotal:</h3>
-          <small>Total:</small>
+          <small>USD {priceFormat(total)}</small>
         </div>
         <div>
           <Link to="/">
